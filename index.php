3c9a2b2b5b2a46cfad8df96330892844d53e0eb3
avlopez-fridays <avlopez@tgifridays.com.hn>
2024-12-10 10:45:51 -0600

    Estilo de alertas

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="assets/css/estilo.css" rel="stylesheet">
-    <link href="assets/css/styles.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">     <!-- Estilo de las alertas -->
 </head>
 <body>
     <!-- Contenedor de Alertas -->
